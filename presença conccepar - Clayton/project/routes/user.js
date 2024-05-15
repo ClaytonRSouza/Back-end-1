1e7a0161197b3c30eb7b006e3c2e54dfe272a218
Clayton Robert <128239110+ClaytonRSouza@users.noreply.github.com>
2024-05-14 22:04:34 -0300

    Add files via upload

---
+++ presença conccepar - Clayton/project/routes/user.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const router = express.Router()
+
+let usuarios = []
+
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+  
+    const usuario = usuarios.find(u => u.email === email && u.password === password);
+    if (usuario) {
+      res.status(200).json({ message: 'Login successful', usuario });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  });
+
+router.post("/register", (req, res) => {
+    const { email, password } = req.body;
+
+    const usuarioExistente = usuarios.find(u => u.email === email);
+    if (usuarioExistente) {
+      res.status(400).json({ message: 'Email already in use' });
+    } else {
+
+      const novoUsuario = {
+        email,
+        password
+      };
+
+      usuarios.push(novoUsuario);
+      res.status(201).json({ message: 'User registered successfully', usuario: novoUsuario });
+    }
+  });router.post("/register", (req, res) => {
+    const {email, password} = req.body
+    if(email === "teste@teste.com" && password === "password"){
+        res.status(200).json({message: 'Logado com sucesso'})
+    }else{
+        res.status(400).json({message: 'e-mail ou senha incorreto'})
+    }
+})
+
+module.exports = {
+    router
+}
